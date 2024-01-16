c9768c6ab6a787d9993085d511afbf8ae9374d9d
Cedric <cedricmarcelino1105@gmail.com>
2024-01-17 06:03:48 +0800

    Added some overrides needed for theme.

--- src/theme.js
+++ src/theme.js
@@ -11,10 +11,35 @@ const theme = createTheme({
         lineHeight: '32px', /* 133.333% */
         letterSpacing: '0.1px',
       },
+      h6: {
+        fontFamily: 'Montserrat',
+        fontSize: '14px',
+        fontStyle: 'normal',
+        fontWeight: '700',
+        lineHeight: '24px', /* 171.429% */
+        letterSpacing: '0.2px',
+      },
     },
     palette: {
       text: {
         primary: '#252B42',
+      }
+    },
+    components: {
+      MuiCard: {
+        styleOverrides:{
+          root: {
+            borderRadius: '0px',
+            boxShadow: 'none',
+          }
+        }
+      },
+      MuiCardContent: {
+        styleOverrides:{
+          root: {
+            padding: '0px',
+          }
+        }
       }
     }
   });
