9f93d2cd4b1b6a8fef553bc8854f86159e695806
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-01-18 05:47:21 +0800

    Update on theme for necessary stylings for Product component

--- src/theme.js
+++ src/theme.js
@@ -19,10 +19,43 @@ const theme = createTheme({
         lineHeight: '24px', /* 171.429% */
         letterSpacing: '0.2px',
       },
+      subtitle1: {
+        fontFamily: 'Montserrat',
+        fontSize: '14px',
+        fontStyle: 'normal',
+        fontWeight: '400',
+        lineHeight: '20px', /* 142.857% */
+        letterSpacing: '0.2px',
+      },
+      subtitle2: {
+        fontFamily: 'Montserrat',
+        fontSize: '14px',
+        fontStyle: 'normal',
+        fontWeight: '700',
+        lineHeight: '24px', /* 171.429% */
+        letterSpacing: '0.2px',
+      },
+      h5: {
+        fontFamily: 'Montserrat',
+        fontSize: '16px',
+        fontStyle: 'normal',
+        fontWeight: '700',
+        lineHeight: '24px', /* 150% */
+        letterSpacing: '0.1px',
+      },
+      button: {
+        fontFamily: 'Montserrat',
+        fontSize: '14px',
+        fontStyle: 'normal',
+        fontWeight: '700',
+        lineHeight: '22px', /* 157.143% */
+        letterSpacing: '0.2px',
+      }
     },
     palette: {
       text: {
         primary: '#252B42',
+        secondary: '#737373',
       }
     },
     components: {
