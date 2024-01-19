ffe460488649dcae41ac00edce172ee494cd5ee4
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-01-19 09:33:46 +0800

    Added layout for Header Desktop View

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Box, Typography } from "@mui/material"
 import styles from './Header.module.css'
-import { SearchIcon, CartIcon, HamburgerIcon, Close } from "../Icons"
+import { SearchIcon, CartIcon, HamburgerIcon, Close, Envelope, Phone, Youtube, Facebook, Twitter, Instagram, Heart, UserIcon } from "../Icons"
 import { useState } from "react"
 import Navigation from "../Navigation/Navigation"
 import React from "react"
@@ -14,21 +14,66 @@ export default function Header() {
     }
     return (
         <>
+        <Box className={styles.headerBanner}>
+            <Box className={styles.contactsContainer}>
+                <Box className={styles.contactNumberContainer}>
+                    <Phone width={16} className={styles.phoneIcon}/>
+                    <Typography variant='h6' color='#FFFFFF'>
+                        (225) 555-0118
+                    </Typography>
+                </Box>
+                <Box className={styles.emailContainer}>
+                    <Envelope width={16} className={styles.envelopeIcon}/>
+                    <Typography variant='h6' color='#FFFFFF'>
+                        michelle.rivera@example.com
+                    </Typography>
+                </Box>
+            </Box>
+            <Typography variant='h6' color='#FFFFFF'>
+                Follow Us  and get a chance to win 80% off
+            </Typography>
+            <Box className={styles.socialsContainer}>
+                <Typography variant='h6' color='#FFFFFF'>
+                    Follow Us :
+                </Typography>
+                <Box className={styles.socialsIconContainer}>
+                    <Instagram className={styles.socialsIcon} width={16}/>
+                    <Youtube className={styles.socialsIcon} width={16}/>
+                    <Facebook className={styles.socialsIcon} width={16}/>
+                    <Twitter className={styles.socialsIcon} width={16}/>
+                </Box>
+            </Box>
+        </Box>
         <Box className={styles.headerRoot}>
             <Box className={styles.brandNameContainer}>
                 <Typography variant='h3'>Bandage</Typography>
             </Box>
-            <Box className={styles.iconContainer}>
-                <SearchIcon className={styles.svgIcon}/>
-                <CartIcon className={styles.svgIcon}/>
-                {isVisible 
-                ? 
-                <Close className={styles.svgIcon} onClick={handleClick}/>
-                :
-                <HamburgerIcon className={styles.svgIcon} onClick={handleClick}/>}
+            <Box className={styles.actionsContainer}>
+                <Navigation setIsVisible={setIsVisible} isVisible={isVisible}/>
+                <Box className={styles.iconContainer}>
+                    <Box className={styles.loginContainer}>
+                        <UserIcon className={styles.userIcon}/>
+                        <Typography variant='h6' color='#23A6F0'>
+                            Login / Register
+                        </Typography>
+                    </Box>
+                    <SearchIcon className={styles.svgIcon}/>
+                    <Box className={styles.cartContainer}>
+                        <CartIcon className={styles.cartIcon}/>
+                        <Typography variant='caption'>0</Typography>
+                    </Box>
+                    {isVisible 
+                    ? 
+                    <Close className={styles.closeIcon} onClick={handleClick}/>
+                    :
+                    <HamburgerIcon className={styles.hamburgerIcon} onClick={handleClick}/>}
+                    <Box className={styles.favoritesContainer}>
+                        <Heart className={styles.heartIcon}/>
+                        <Typography variant='caption'>0</Typography>
+                    </Box>
+                </Box>
             </Box>
         </Box>
-        <Navigation setIsVisible={setIsVisible} isVisible={isVisible}/>
         </>
     )
   }
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,6 +3,7 @@ import { Box, Typography } from "@mui/material"
 import styles from './Navigation.module.css'
 import ReactDOM from 'react-dom';
 import { MutableRefObject, useEffect, useRef } from "react";
+import { ChevronDown } from "../Icons";
 
 interface NavigationProps {
     isVisible: boolean;
@@ -11,21 +12,46 @@ interface NavigationProps {
 
 export default function Navigation (props: NavigationProps) {
     const { isVisible, setIsVisible } = props;
-    const wrapperRef = useRef(null);
-    return ( isVisible &&
-        <Box className={styles.navMenu} ref={wrapperRef}>
-            <Typography className={styles.navLink}>
-                Home
-            </Typography>
-            <Typography className={styles.navLink}>
-                Product
-            </Typography>
-            <Typography className={styles.navLink}>
-                Pricing
-            </Typography>
-            <Typography className={styles.navLink}>
-                Contact
-            </Typography>
-        </Box>
+    return ( 
+        <>
+            <Box className={styles.navMenuDesktop}>
+                <Typography variant='subtitle2' color='textSecondary'>
+                    Home
+                </Typography>
+                <Box className={styles.shopContainer}>
+                    <Typography variant='subtitle2' color='textSecondary'>
+                        Shop
+                    </Typography>
+                    <ChevronDown width={10} height={15}/>
+                </Box>
+                <Typography variant='subtitle2' color='textSecondary'>
+                    About
+                </Typography>
+                <Typography variant='subtitle2' color='textSecondary'>
+                    Blog
+                </Typography>
+                <Typography variant='subtitle2' color='textSecondary'>
+                    Contact
+                </Typography>
+                <Typography variant='subtitle2' color='textSecondary'>
+                    Pages
+                </Typography>
+            </Box>
+            {isVisible &&
+            <Box className={styles.navMenu}>
+                <Typography className={styles.navLink}>
+                    Home
+                </Typography>
+                <Typography className={styles.navLink}>
+                    Product
+                </Typography>
+                <Typography className={styles.navLink}>
+                    Pricing
+                </Typography>
+                <Typography className={styles.navLink}>
+                    Contact
+                </Typography>
+            </Box>}
+        </>
     )
 }
