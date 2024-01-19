f3d3f265c910732a708789f1e6d75e6a675980bb
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-01-20 06:12:32 +0800

    added new icons and sponsor component

---
+++ src/components/Sponsors/Sponsors.tsx
@@ -0,0 +1,16 @@
+import { Aws, Hooli, Lyft, PiedPiperHat, Reddit, Stripe } from "../Icons";
+import { Box } from "@mui/material";
+import styles from './Sponsors.module.css'
+
+export default function Sponsors() {
+    return (
+        <Box className={styles.sponsorsContainer}>
+            <Hooli/>
+            <Lyft/>
+            <PiedPiperHat/>
+            <Stripe/>
+            <Aws/>
+            <Reddit/>
+        </Box>
+    )
+}
