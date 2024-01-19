73868121316407524f495230cb4afc0b2c7d97c4
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-01-20 06:13:36 +0800

    added product page

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,29 @@
+import Product from '@/components/Product/Product'
+import { Product as IProduct } from '@/redux/features/productsSlice'
+import Layout from '@/components/RootLayout/Layout'
+import { GetServerSidePropsContext } from 'next/types';
+
+  
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+const { id } = context.query;
+const res = await fetch(`https://dummyjson.com/products/${id}`);
+const product = await res.json();
+return { props: product };
+}
+
+export default function Page(props: IProduct)  {
+    // console.log(props);
+    return (
+        <Product product={props}/>
+    )
+}
+
+Page.getLayout = function getLayout(page: React.ReactNode) {
+    return (
+    <>
+      <Layout>
+          {page}
+      </Layout>
+    </>
+    )
+  }
