b33fbf8d85a473ab39e6b88492171cfd61faede9
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-01-21 00:20:09 +0800

    added additional styling for handling empty cart when on product page

--- src/components/RootLayout/Layout.tsx
+++ src/components/RootLayout/Layout.tsx
@@ -4,15 +4,19 @@ import Footer from '../Footer/Footer'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '@/theme';
+import { Box } from "@mui/material";
+import styles from './Layout.module.css'
 
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <StoreProvider>
       <AppRouterCacheProvider>
         <ThemeProvider theme={theme}>
-          <Header/>
-          {children}
-          <Footer/>
+          <Box className={styles.pageWrapper}>
+            <Header/>
+            {children}
+            <Footer/>
+          </Box>
         </ThemeProvider>
       </AppRouterCacheProvider>
     </StoreProvider>
