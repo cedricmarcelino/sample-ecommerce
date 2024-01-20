4300139b96f5e98304f494e28818367cd368e5af
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2024-01-21 00:21:19 +0800

    added initialization of cart from localstorage to redux

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,51 @@ import Posts from '@/components/Posts/Posts';
 import Products from '@/components/Products/Products';
 import Services from '@/components/Services/Services';
 import Testimonials from '@/components/Testimonials/Testimonials';
+import Cart from '@/components/Cart/Cart';
+import { useAppSelector } from "@/redux/hooks";
+import { Box, Snackbar } from "@mui/material";
+import styles from './index.module.css'
+import { useEffect } from 'react';
+import { useAppDispatch } from '@/redux/hooks';
+import { setCart } from '@/redux/features/cartSlice';
+import { CartContents, LOCAL_STORAGE_KEYS } from '@/types/types';
+import { getLocalStorage } from '@/utilities/utilities';
+import { closeSnackbar } from '@/redux/features/snackbarSlice';
  
 export default function Page() {
+  const isCartOpen = useAppSelector((state) => state.cartReducer.cart.isOpen)
+  const isSnackbarOpen = useAppSelector((state) => state.snackbarReducer.snackbar.isOpen)
+  const snackbarMessage = useAppSelector((state) => state.snackbarReducer.snackbar.message)
+  const dispatch = useAppDispatch()
+
+  const handleSnackbarClose = () => {
+    dispatch(closeSnackbar())
+  }
+
+  useEffect(() => {
+    // Initialize redux states:
+    const cartContents: CartContents = JSON.parse(getLocalStorage(LOCAL_STORAGE_KEYS.CART) as string)
+    dispatch(setCart(cartContents))
+  }, [])
+
   return (
-    <>
-      <Categories/>
-      <Products/>
-      <Services/>
-      <Posts/>
-      <Testimonials/>
-      <Banner/>
-    </>
+    <Box>
+      {isCartOpen && <Cart/>}
+      <Box className={isCartOpen ? styles.hideComponents : styles.showComponents}>
+        <Categories/>
+        <Products/>
+        <Services/>
+        <Posts/>
+        <Testimonials/>
+        <Banner/>
+        <Snackbar 
+          open={isSnackbarOpen}
+          autoHideDuration={2000}
+          onClose={handleSnackbarClose}
+          message={snackbarMessage}
+        />
+      </Box>
+    </Box>
   )
 }
  
